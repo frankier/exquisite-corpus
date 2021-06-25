166d5f568a5a4dc382875381c9531b2031deb72f
Frankie Robertson <frankie@robertson.name>
2021-06-25 11:12:52 +0300

    Remove version guard on Snakemake

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     packages=['exquisite_corpus'],
     include_package_data=True,
     install_requires=[
-        'snakemake < 5.6', 'jieba >= 0.42', 'wordfreq[jieba,mecab] >= 2.3.2',
+        'snakemake', 'jieba >= 0.42', 'wordfreq[jieba,mecab] >= 2.3.2',
         'click', 'regex >= 2020.04.04', 'pycld2', 'msgpack-python',
         'ordered-set', 'ftfy', 'subword-nmt', 'sentencepiece==0.1.86', 'mmh3',
         'pytest', 'tqdm', 'lumi-language-id', 'zstandard', 'langcodes[data] >= 2.1',
